ac81a7aec8cec3443ca2ce95b48abdcf4ecb956d
WooJoongKim <rlandwnd555@naver.com>
2023-05-20 14:38:59 +0900

    inital commit

---
+++ src/main/java/com/barbel/streamingserver/StreamingserverApplication.java
@@ -0,0 +1,13 @@
+package com.barbel.streamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StreamingserverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/barbel/streamingserver/StreamingserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbel.streamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
