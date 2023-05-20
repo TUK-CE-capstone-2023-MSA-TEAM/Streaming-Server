5cd33760997d2f7e9732bd0714bd7a2cf0224f7d
WooJoongKim <rlandwnd555@naver.com>
2023-05-20 15:51:35 +0900

    feat : Exception Handler 추가

---
+++ src/main/java/com/barbel/streamingserver/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.barbel.streamingserver.global.error;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import com.barbel.memberserver.global.error.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler
+  public ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
+    final ErrorCode errorCode = e.getErrorCode();
+    final ErrorResponse response = ErrorResponse.builder()
+        .message(errorCode.getMessage())
+        .code(errorCode.getCode())
+        .build();
+    log.warn(e.getMessage());
+    return ResponseEntity.status(errorCode.getStatus()).body(response);
+  }
+
+  @ExceptionHandler
+  protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
+    final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+    log.warn(e.getMessage());
+    return new ResponseEntity<>(response, BAD_REQUEST);
+  }
+}
