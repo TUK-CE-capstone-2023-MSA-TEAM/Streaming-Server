2f0bbf01d0077ce108986fd899f65a96c804ca61
WooJoongKim <rlandwnd555@naver.com>
2023-05-24 23:19:44 +0900

    fix : 빌더 패턴 추가 및 썸네일 이미지 URL 필드 추가

--- src/main/java/com/barbel/streamingserver/domain/videoGroup/document/VODGroup.java
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/document/VODGroup.java
@@ -2,6 +2,8 @@ package com.barbel.streamingserver.domain.videoGroup.document;
 
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -9,12 +11,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 @Document(collection = "video_group")
 public class VODGroup {
   @Id
   private String _id;
   private String ownerId;       // 소유자 ID
   private String vodGroupName;  // VOD 그룹 이름 (인강 시리즈 제목)
-  private String vodCount;      // VOD 그룹에 포함된 VOD 갯수
+  private int vodCount;      // VOD 그룹에 포함된 VOD 갯수
+  private String thumbnailURL; // 썸네일 이미지 URL
   private List<VOD> VODList;   // VOD 리스트
 }
