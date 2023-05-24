ea3ef2288b1f1d2bba636670ec191a5429ebcd61
WooJoongKim <rlandwnd555@naver.com>
2023-05-24 23:19:11 +0900

    feat : VODGroup 관련 응답 코드 추가

--- src/main/java/com/barbel/streamingserver/global/result/ResultCode.java
+++ src/main/java/com/barbel/streamingserver/global/result/ResultCode.java
@@ -8,9 +8,16 @@ import lombok.Getter;
 public enum ResultCode {
 
   // Global
-  MEMBER_AUTHOR_SUCCESS("G001", "회원 인증 성공");
+  MEMBER_AUTHOR_SUCCESS("G001", "회원 인증 성공"),
 
-  //VIDEO
+  //VODGroup
+  VODGROUP_REGISTRATION_SUCCESS("V001", "VOD 그룹 등록 성공"),
+  VODGROUP_UPDATE_SUCCESS("V002", "VOD 그룹 수정 성공"),
+  VODGROUP_SEARCH_SUCCESS("V003", "VOD 그룹 조회 성공"),
+  VODGROUP_DELETE_SUCCESS("V004", "VOD 그룹 삭제 성공"),
+  VODGROUP_LIST_REQUEST_SUCCESS("V005", "VOD 그룹 리스트 조회 성공"),
+
+  ;
 
 
   private final String code;
