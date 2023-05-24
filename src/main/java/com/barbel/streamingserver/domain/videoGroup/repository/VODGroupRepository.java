22ca9acad76017bc4b240c70284d04733fd0a20a
WooJoongKim <rlandwnd555@naver.com>
2023-05-24 23:21:51 +0900

    feat : VOD그룹 레포지토리 및 ownerId로 탐색하는 쿼리 메서드 추가

---
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/repository/VODGroupRepository.java
@@ -0,0 +1,11 @@
+package com.barbel.streamingserver.domain.videoGroup.repository;
+
+import com.barbel.streamingserver.domain.videoGroup.document.VODGroup;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VODGroupRepository extends MongoRepository<VODGroup, String> {
+  List<VODGroup> findAllByOwnerId(String ownerId);
+}
