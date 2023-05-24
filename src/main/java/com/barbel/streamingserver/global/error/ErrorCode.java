c34dec0ab5394db57271609896263078c88f73d1
WooJoongKim <rlandwnd555@naver.com>
2023-05-24 23:19:03 +0900

    feat : VODGroup 관련 에러 코드 추가

--- src/main/java/com/barbel/streamingserver/global/error/ErrorCode.java
+++ src/main/java/com/barbel/streamingserver/global/error/ErrorCode.java
@@ -12,8 +12,12 @@ public enum ErrorCode {
   INTERNAL_SERVER_ERROR(500, "G002", "서버 내부 오류입니다."),
   FILE_CONVERT_ERROR(500, "G003", "파일 변환 오류입니다."),
   UN_AUTHORIZED_ACCESS(401, "G004", "권한이 없습니다."),
-  INVALID_TOKEN(401, "G005", "유효하지 않은 토큰입니다.");
+  INVALID_TOKEN(401, "G005", "유효하지 않은 토큰입니다."),
 
+  //VODGroup
+  VODGROUP_NOT_FOUND(404, "V001", "VOD 그룹을 찾을 수 없습니다."),
+
+  ;
 
   private final int status;
   private final String code;
