73ba1c3299c3ecbe5bcebbcc6b526ac3e873b006
Identityless <rlandwnd555@naver.com>
2023-05-26 17:42:38 +0900

    fix : VOD 제목 필드 이름 title로 수정

--- src/main/java/com/barbel/streamingserver/domain/videoGroup/document/VOD.java
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/document/VOD.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class VOD {
   private int idx;
-  private String vodName;       // VOD 제목
+  private String title;       // VOD 제목
   private String vodType;       // VOD 동영상 파일 확장자
   private String vodURL;        // VOD 스토리지 URL
   private int vodLength;        // VOD 길이 (초)
