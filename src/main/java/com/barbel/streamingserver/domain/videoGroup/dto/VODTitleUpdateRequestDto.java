e6a53fd4c2641edbd4eb27770a74b68e9c303006
Identityless <rlandwnd555@naver.com>
2023-05-26 17:43:40 +0900

    feat : VOD 제목 수정 요청 DTO 추가

---
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/dto/VODTitleUpdateRequestDto.java
@@ -0,0 +1,9 @@
+package com.barbel.streamingserver.domain.videoGroup.dto;
+
+import lombok.Data;
+
+@Data
+public class VODTitleUpdateRequestDto {
+    private String id;
+    private String title;
+}
