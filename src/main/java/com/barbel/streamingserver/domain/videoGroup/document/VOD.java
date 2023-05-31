2403a93c2f21252dfecc69a06a59515906c2d035
Identityless <rlandwnd555@naver.com>
2023-05-31 18:08:10 +0900

    fix : 일부 필드 수정

--- src/main/java/com/barbel/streamingserver/domain/videoGroup/document/VOD.java
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/document/VOD.java
@@ -9,6 +9,8 @@ public class VOD {
   private int idx;
   private String title;       // VOD 제목
   private String vodType;       // VOD 동영상 파일 확장자
+  private String description;   // VOD 설명
   private String vodURL;        // VOD 스토리지 URL
-  private int vodLength;        // VOD 길이 (초)
+  private String thumbnailURL;  // VOD 썸네일 스토리지 URL
+//  private int vodLength;        // VOD 길이 (초)
 }
