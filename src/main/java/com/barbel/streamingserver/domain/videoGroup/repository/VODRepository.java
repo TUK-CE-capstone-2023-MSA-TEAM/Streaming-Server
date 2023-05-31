5a56e5d813e4779f38144b30cfa41d73918973df
Identityless <rlandwnd555@naver.com>
2023-05-31 18:11:00 +0900

    remove : 미 사용 클래스 삭제

--- src/main/java/com/barbel/streamingserver/domain/videoGroup/repository/VODRepository.java
+++
@@ -1,9 +0,0 @@
-package com.barbel.streamingserver.domain.videoGroup.repository;
-
-import com.barbel.streamingserver.domain.videoGroup.document.VOD;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VODRepository extends MongoRepository<VOD, String> {
-}
