9687689190e6611702d3f470da39061813251d72
Identityless <rlandwnd555@naver.com>
2023-05-31 18:07:47 +0900

    feat : VOD 관련 응답 코드 추가

--- src/main/java/com/barbel/streamingserver/global/result/ResultCode.java
+++ src/main/java/com/barbel/streamingserver/global/result/ResultCode.java
@@ -17,6 +17,11 @@ public enum ResultCode {
   VODGROUP_DELETE_SUCCESS("V004", "VOD 그룹 삭제 성공"),
   VODGROUP_LIST_REQUEST_SUCCESS("V005", "VOD 그룹 리스트 조회 성공"),
 
+  //VOD
+  VOD_REGISTRATION_INIT_SUCCESS("V006", "VOD 등록 초기화 성공"),
+  VOD_MULTIPART_UPLOAD_SUCCESS("V007", "VOD 멀티파트 업로드 성공"),
+  VOD_UPLOAD_COMPLATE("V007", "VOD 업로드 완료"),
+
   ;
 
 
