af060a40b3b2172e5936653e4ad8885bb692195c
WooJoong Kim <rlandwnd555@naver.com>
2023-06-19 21:34:30 +0900

    comment : 주석 추가

--- src/main/java/com/barbel/streamingserver/domain/videoGroup/dto/VODRegistrationRequestDto.java
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/dto/VODRegistrationRequestDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class VODRegistrationRequestDto {
-    private String title;
+    private String title; // VOD 제목 (확장자 포함)
     private String description;
     private String VODGroupId;
     private int vodIndex;
