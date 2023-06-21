0e430fa1e1adc7e22458a16e9c375b0395448983
WooJoong Kim <rlandwnd555@naver.com>
2023-06-21 21:50:28 +0900

    fix : 키워드 필드 추가

--- src/main/java/com/barbel/streamingserver/domain/videoGroup/dto/VODGroupRegistrationRequestDto.java
+++ src/main/java/com/barbel/streamingserver/domain/videoGroup/dto/VODGroupRegistrationRequestDto.java
@@ -7,4 +7,5 @@ import lombok.Data;
 public class VODGroupRegistrationRequestDto {
   private String vodGroupName;   // VOD 그룹 이름
   private String ownerId;        // 소유자 ID
+  private String keyword;         // 검색 키워드
 }
